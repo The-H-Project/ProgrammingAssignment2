5ed14e6ef8843c5b355600304be9fb111b684b97
Harris Lam <harris.lam@gmail.com>
2015-08-21 19:41:10 -0400

    First commit test

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## R Programming Programming Assignment 2 (Week 3)
 
 ## Write a short comment describing this function
+# makeCacheMatrix: a Function of four parts:
+# $setMatrix: sets the matrix values
+# $getMatrix: gets the set matrix values, if existing
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  setMatrix <- function (y) {
+       x <<- y
+  }
+  list (setMatrix = setMatrix)  
+  
 }
 
 
