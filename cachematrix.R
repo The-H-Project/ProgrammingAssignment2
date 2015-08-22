c9feac070b3b54aea22d15c77cf90614ebc5ce8e
Harris Lam <harris.lam@gmail.com>
2015-08-22 12:43:12 -0400

    Finished cache flag checking and function documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,19 @@
 ## R Programming Programming Assignment 2 (Week 3)
-## Version 20150821
+## Version Date: 20150822
 
 # makeCacheMatrix: a Function of two parts:
 # $get: gets the set matrix values, if existing
 # $set: overwrite cached matrix values
 
 ## To use makeCacheMatrix
-## 1. Assign create a matrix.
+## 1. Create a matrix.
 ##    > testMatrix <- makeCacheMatrix(matrix(1:9,3,3))
-## 2. Get the assigned matrix.
+## 2. Display the assigned matrix.
 ##    > testMatrix$get()
 ## 3. Overwrite the assigned matrix with a new one.
 ##    > testMatrix$set(matrix(1:4,2,2))
+## 4. Display the newly assigned matrix.
+##    > testMatrix$get()
 makeCacheMatrix <- function(cachedMatrix = matrix()) {
   
   # Return the stored matrix.
@@ -28,12 +30,21 @@ makeCacheMatrix <- function(cachedMatrix = matrix()) {
 
 
 ## cacheSolve: computes the inverse of the matrix held in makeCacheMatrix
-## Use: aniMatrix <- cacheSolve(testMatrix$get())
+## To use cacheSolve (continuing step numbers from above):
+## 5. Get the inverse of the matrix held the cache.
+## > aniMatrix <- cacheSolve(testMatrix$get())
+## 6. Do it again to get the result from the cache.
+## > aniMatrix <- cacheSolve(testMatrix$get())
+## 7. Change the matrix to force the inverse cache to recalculate.
+## > testMatrix$set(matrix(1:12,3,4))
+## 8. Get the inverse of the matrix held the cache.
+## > aniMatrix <- cacheSolve(testMatrix$get())
 
 cacheSolve <- function(cachedMatrix, ...) {
   
   # Provide the cached inverse matrix if available.
   if (!is.null(inversedMatrix)) {
+     message ('Returning inverse from cache')
      return (inversedMatrix)
   }
   # Otherwise, attempt to solve for the inverse. 
