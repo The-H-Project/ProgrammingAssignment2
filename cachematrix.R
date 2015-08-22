89adc3e4ccb77500fd889bf3a0d0b83ffe13a490
Harris Lam <harris.lam@gmail.com>
2015-08-21 22:21:45 -0400

    Added basic cachesolve functionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,39 @@
 ## R Programming Programming Assignment 2 (Week 3)
 ## Version 20150821
 
-# makeCacheMatrix: a Function of four parts:
+# makeCacheMatrix: a Function of two parts:
 # $get: gets the set matrix values, if existing
 # $set: overwrite cached matrix values
 
 ## To use makeCacheMatrix
 ## 1. Assign create a matrix.
-##    testMatrix <- makeCacheMatrix(matrix(1:4,2,2))
-
+##    > testMatrix <- makeCacheMatrix(matrix(1:9,3,3))
+## 2. Get the assigned matrix.
+##    > testMatrix$get()
+## 3. Overwrite the assigned matrix with a new one.
+##    > ## testMatrix$set(matrix(1:4,2,2))
 makeCacheMatrix <- function(cachedMatrix = matrix()) {
   
   # Wipe out any stored Inverse Matrix values.
   inverseMatrix <- NULL
   
+  # Return the stored matrix.
   get <- function () cachedMatrix
   
+  # Overwrite the stored matrix with a new one.
   set <- function (y) {
        cachedMatrix <<- y
   }
   
-  
-  
-  
-  
   list (get = get, set = set)  
-  
 }
 
 
-## cacheSolve
+## cacheSolve: computes the inverse of the matrix held in makeCacheMatrix
+## Use: aniMatrix <- cacheSolve(testMatrix$get())
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachedMatrix, ...) {
+  
+  inversedMatrix <- solve(cachedMatrix)
+  
 }
